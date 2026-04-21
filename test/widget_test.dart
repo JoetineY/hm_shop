74d036fedc28673a980b8e2bc69c06992ec76fb7
JoetineY <952068491@qq.com>
2026-04-21 11:31:03 +0800

    搭建基础路由和组件

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hm_shop/main.dart';
+import 'package:hm_shop/main_init.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
