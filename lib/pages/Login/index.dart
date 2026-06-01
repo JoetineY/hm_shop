6600ec293bf9ac1f78b104c12f0f07dc044a3486
joetine <952068491@qq.com>
2026-06-01 22:07:55 +0800

    登录页面搭建和表单校验

--- lib/pages/Login/index.dart
+++ lib/pages/Login/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/utils/toast_utils.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -8,8 +9,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController _phoneController = TextEditingController(); // 账号控制器
-  TextEditingController _codeController = TextEditingController(); // 密码控制器
+  final TextEditingController _phoneController =
+      TextEditingController(); // 账号控制器
+  final TextEditingController _codeController =
+      TextEditingController(); // 密码控制器
+
+  final GlobalKey<FormState> _key = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +24,7 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.white,
       ),
       body: Form(
+        key: _key,
         child: Container(
           padding: EdgeInsets.all(30),
           color: Colors.white,
@@ -41,10 +47,35 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  // 头部Widget
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            "账号密码登录",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
   // 用户账号Widget
   Widget _buildPhoneTextField() {
     return TextFormField(
       controller: _phoneController,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "账号不可以为空";
+        }
+        // 校验账号格式
+        if (!RegExp(r"^1[3-9]\d{9}$").hasMatch(value)) {
+          return "账号（手机号）格式错误";
+        }
+        return null;
+      },
       decoration: InputDecoration(
         contentPadding: EdgeInsets.only(left: 20), // 内容内边距
         hintText: "请输入账号",
@@ -62,7 +93,17 @@ class _LoginPageState extends State<LoginPage> {
   Widget _buildCodeTextField() {
     return TextFormField(
       controller: _codeController,
-      obscureText: true,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "密码不可以为空";
+        }
+        // 校验密码格式，6-16位数字/字幕/下划线
+        if (!RegExp(r"^[0-9a-zA-Z_]{6,16}$").hasMatch(value)) {
+          return "密码格式错误, 格式: 6-16位数字/字幕/下划线";
+        }
+        return null;
+      },
+      obscureText: true, // 密文显示
       decoration: InputDecoration(
         contentPadding: EdgeInsets.only(left: 20), // 内容内边距
         hintText: "请输入密码",
@@ -72,26 +113,6 @@ class _LoginPageState extends State<LoginPage> {
           borderSide: BorderSide.none,
           borderRadius: BorderRadius.circular(25),
         ),
-      ),
-    );
-  }
-
-  // 登录按钮Widget
-  Widget _buildLoginButton() {
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: () {
-          // 登录逻辑
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
-        ),
-        child: Text("登录", style: TextStyle(fontSize: 18, color: Colors.white)),
       ),
     );
   }
@@ -106,7 +127,10 @@ class _LoginPageState extends State<LoginPage> {
           value: _isChecked,
           activeColor: Colors.black,
           checkColor: Colors.white,
-          onChanged: (bool? value) {},
+          onChanged: (bool? value) {
+            _isChecked = value ?? false;
+            setState(() {});
+          },
           // 设置形状
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10), // 圆角大小
@@ -134,18 +158,31 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  // 头部Widget
-  Widget _buildHeader() {
-    return Row(
-      children: [
-        Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: Text(
-            "账号密码登录",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+  // 登录按钮Widget
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          // 登录逻辑
+          if (_key.currentState!.validate()) {
+            // 进行勾选框的判断
+            if (_isChecked) {
+            } else {
+              ToastUtils.showToast(context, "请勾选用户协议");
+            }
+          }
+          ;
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
           ),
         ),
-      ],
+        child: Text("登录", style: TextStyle(fontSize: 18, color: Colors.white)),
+      ),
     );
   }
 }
--- lib/utils/toast_utils.dart
+++ lib/utils/toast_utils.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 // 轻提示组件
 class ToastUtils {
+  // 阀门控制，避免提示框显示频率跟不上点击频率
+  static bool showLoading = false;
   static void showToast(BuildContext context, String? msg) {
+    if (showLoading) {
+      return;
+    }
+    showLoading = true;
+    Future.delayed(Duration(seconds: 3), () {
+      showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        width: 120,
+        width: 180,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(40),
         ),
